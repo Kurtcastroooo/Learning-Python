9d536170d27a07944d234bb53cde1735a7dfa157
Kato Riamu <82269848+Kurtcastroooo@users.noreply.github.com>
2025-06-28 15:28:04 +0800

    updates

---
+++ arithmetic_math_functions.py
@@ -0,0 +1,49 @@
+# Math
+
+friends = 1
+
+friends = friends + 1
+
+friends += 1
+
+friends = friends - 1
+
+friends -= 1
+
+friends *= 3
+
+friends /= 2
+
+friends = friends **2
+
+friends **=2
+
+remainder = friends % 2
+
+# print(remainder)
+
+# Math Functions
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)
+
+result = abs(y)
+
+result = pow(12,3)
+
+result = max(x,y,z)
+
+result = min(x,y,z)
+
+# print(result)
+
+import math as mt
+
+print(mt.pi)
+print(mt.e)
+print(mt.sqrt(y))
+print(mt.ceil(x))
+print(mt.floor(x))
