83494e7bfed3608f1d1e281a4d41f7156463ff8e
Kato Riamu <82269848+Kurtcastroooo@users.noreply.github.com>
2025-06-28 15:27:26 +0800

    updates

---
+++ condtional_expressions.py
@@ -0,0 +1,5 @@
+# conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#                          print or assign one of two values based on a condition
+#                          x if condition else y
+
+
