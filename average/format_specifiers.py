7655b0f181e9a170f82fc04b6dd576ca889d80c5
Kurtcastroooo <kurtliam.castro@gmail.com>
2025-08-23 01:03:42 +0800

    commit

---
+++ average/format_specifiers.py
@@ -0,0 +1,14 @@
+format specifiers = {:flags} format a value based on what flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocated that many spaces
+# :03 = allocated and zero pad that many spaces
+# :< (number) = left align within allocated spaces 
+# :> (number) = right align within allocated spaces
+# :^ (number) = center align within allocated spaces
+# + = use a plus sign for positive numbers
+# := = place the sign to the left most position
+# : = insert a space before positive numbers
+# , = use a comma as a thousand separator
+
+
